e3c70f138c09aef20a14989a929fa685b01a266b
Josias Besodia <djodev23@gmail.com>
2025-02-14 15:36:18 +0100

    controller role setup

--- backend/controllers/roleController.js
+++ backend/controllers/roleController.js
@@ -15,4 +15,43 @@ exports.createRole = async (req, res) => {
         console.log(`Une erreur s'est produite, ${err}`);
         return res.status(500).json({ message: 'Un problème est survenu'});
     }
-}
+};
+
+exports.getRole = async (req, res) => {
+    try {
+        const role = await Role.findByPk(req.params.id);
+
+        if (!role) return res.status(404).json({ error : 'Role non trouvé '});
+        res.json(role);
+    } catch (err) {
+        console.log(`Une erreur s'est produite lors de l'ajout du role, ${err}`);
+        return res.status(500).json({ message: 'Une erreur s\'est produite'});
+    }
+};
+
+exports.updateRole = async (req, res) => {
+    try {
+        const role = await Role.findByPk(req.params.id);
+
+        if (!role) return res.status(404).json({ error: 'Aucun role trouvé'});
+
+        await role.update(req.body);
+        res.json({ message: 'Role mis à jour avec succès'});
+    } catch(err) {
+        console.log(`Erreur lors de la modification du role, ${err}`);
+        res.status(500).json({ error: 'Erreur lors de la modification du role'});
+    } 
+};
+
+exports.deleteRole = async (req, res) => {
+    try {
+        const role = await Role.findByPk(req.params.id);
+        if (!role) return res.status(404).json({error: 'L\'élement n\'existe pas !!!! '});
+
+        await role.destroy();
+        res.json({ message: 'Role supprimé avec succès'});
+    } catch (err) {
+        console.log(`Erreur lors de la supression du role, ${err}`);
+        res.status(500).json({ error: 'Erreur lors de la suppression du role'});
+    }
+};
