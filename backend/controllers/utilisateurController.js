ab29a5dde35a24522344954e57936d8a9f5c21f6
Josias Besodia <djodev23@gmail.com>
2025-02-14 13:07:23 +0100

    add user successful

--- backend/controllers/utilisateurController.js
+++ backend/controllers/utilisateurController.js
@@ -1,10 +1,21 @@
+const passport = require('passport');
 const Utilisateur = require('../models/Utilisateurs');
+const bcrypt = require("bcrypt");
 
 exports.createUtilisateur = async (req, res) => {
     try {
-        const { nom, email, mot_de_passe, role } = req.body;
-        const nouvelUtilisateur = await Utilisateur.create({ nom, email, mot_de_passe, role });
-        res.status(201).json(nouvelUtilisateur);
+        let { nom, prenom, email, default_mot_de_passe, role_id } = req.body;
+
+        const UtilisateurExiste  = await Utilisateur.findOne({ where: { email }});
+        if (UtilisateurExiste) {
+            return res.status(500).json({ error : 'Cet Email existe déjà ' });
+        }
+
+        const saltRounds = 10;
+        const hashed_mot_de_passe = await bcrypt.hash(default_mot_de_passe, saltRounds);
+        const nouvelUtilisateur = await Utilisateur.create({ nom, prenom, email, mot_de_passe: hashed_mot_de_passe, role_id });
+        console.log(`Nouvel utilisateur ajouter avec succès }`);
+        res.status(201).json({message : 'Nouvel utilisateur ajouter avec succès ! ', nouvelUtilisateur });
     } catch (err) {
         console.log(`Erreur lors de la création de l\'utilisateur, ${err}`)
         res.status(500).json({ error: 'Erreur lors de la création de l\'utilisateur' });
