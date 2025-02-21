c80766d85ecaea701d79e16faf6ee6c2eab6255a
Josias Besodia <djodev23@gmail.com>
2025-02-21 12:40:00 +0100

    backend test new users

--- backend/routes/UtilisateursRoutes.js
+++ backend/routes/UtilisateursRoutes.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const utilisateurController = require('../controllers/utilisateurController');
 
 router.post('/signup', utilisateurController.createUtilisateur);
-router.post('/signin', utilisateurController.getUtilisateur);
+router.get('/:email', utilisateurController.getUtilisateur);
 router.put('/:id', utilisateurController.updateUtilisateur);
 router.delete('/:id', utilisateurController.deleteUtilisateur);
 
