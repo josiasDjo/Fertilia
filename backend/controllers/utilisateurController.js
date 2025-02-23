c1f17b97ab7eaf541e9730c9778621ab303da218
Josias Besodia <djodev23@gmail.com>
2025-02-23 18:14:01 +0100

    change4

--- backend/controllers/utilisateurController.js
+++ backend/controllers/utilisateurController.js
@@ -8,14 +8,14 @@ exports.createUtilisateur = async (req, res) => {
         const role_id = 1;
         const UtilisateurExiste  = await Utilisateur.findOne({ where: { email }});
         if (UtilisateurExiste) {
-            return res.status(500).json({ error : 'Cet Email existe déjà ' });
+            return res.json({ success:false, message : 'Cet Email existe déjà ' });
         }
 
         const saltRounds = 10;
         const hashed_mot_de_passe = await bcrypt.hash(default_mot_de_passe, saltRounds);
         const nouvelUtilisateur = await Utilisateur.create({ nom, prenom, email, mot_de_passe: hashed_mot_de_passe, role_id });
         console.log(`Nouvel utilisateur ajouter avec succès }`);
-        res.status(201).json({message : 'Nouvel utilisateur ajouter avec succès ! ', nouvelUtilisateur });
+        res.json({ success: true, message : 'Nouvel utilisateur ajouter avec succès ! ', nouvelUtilisateur });
     } catch (err) {
         console.log(`Erreur lors de la création de l\'utilisateur, ${err}`)
         res.status(500).json({ error: 'Erreur lors de la création de l\'utilisateur' });
