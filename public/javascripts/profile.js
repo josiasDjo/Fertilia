0888a370763a79f1f00cd6cde9467f68f46948d1
Josias Besodia <djodev23@gmail.com>
2025-02-25 20:18:57 +0100

    change13

--- public/javascripts/profile.js
+++ public/javascripts/profile.js
@@ -4,18 +4,29 @@ document.getElementById('connexion_page_send').addEventListener("submit", async 
     const email = document.getElementById('email_signin').value;
     const mot_de_passe = document.getElementById('password_signin').value;
 
-    const response = await fetch("/api/utilisateurs/signin", {
-        method: "POST",
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify({ email, mot_de_passe})
-    });
+    if (email != "" && mot_de_passe != "") {
+        const response = await fetch("/api/utilisateurs/signin", {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify({ email, mot_de_passe})
+        });
 
-    const data = await response.json();
-    const message_error = document.getElementById('message_error');
-    if (data.success) {
-        window.location.href = '/users/mon-profile';
+        const data = await response.json();
+        const message_error = document.getElementById('message_error');
+        if (data.success) {
+            window.location.href = '/users/mon-profile';
+        } else {
+            message_error.innerText = data.message;
+        }
     } else {
-        message_error.innerText = data.message;
+        // alert('Tous les champs sont réquis');
+        let err_msg = "Tous les champs sont réquis";
+        const message_error = document.getElementById('message_error');
+        if (message_error) {
+            message_error.innerText = err_msg;
+        }else {
+            alert('Message error not found');
+        } 
     }
 });
 
@@ -46,7 +57,7 @@ document.getElementById('inscription_page_submit').addEventListener('submit', as
             message_error.innerText = data.message;
         }
     } else {
-        alert('Tous les champs sont réquis');
+        // alert('Tous les champs sont réquis');
         let err_msg = "Tous les champs sont réquis";
         const message_error = document.getElementById('message_error');
         if (message_error) {
