ce9e7065260cfbfd5221f15b77131e4d7d86b2b7
Josias Besodia <djodev23@gmail.com>
2025-02-26 09:34:20 +0100

    change9

--- public/javascripts/profile.js
+++ public/javascripts/profile.js
@@ -3,7 +3,6 @@ document.getElementById('connexion_page_send').addEventListener("submit", async 
 
     const email = document.getElementById('email_signin').value;
     const mot_de_passe = document.getElementById('password_signin').value;
-    const message_error_in = document.getElementById('message_error_in');
 
     if (email != "" && mot_de_passe != "") {
         const response = await fetch("/api/utilisateurs/signin", {
@@ -13,26 +12,28 @@ document.getElementById('connexion_page_send').addEventListener("submit", async 
         });
 
         const data = await response.json();
+        const message_error = document.getElementById('message_error');
         if (data.success) {
             try {
                 alert('G : ', data.message);
-                message_error_in.innerText = data.message;
-                message_error_in.style.color = "green";
+                message_error.innerText = data.message;
+                message_error.style.color = "green";
                 window.location.href = "/users/mon-profile";
             } catch (err) {
                 console.log('Une erreur est survenue, ', err);
             }
         } else {
             alert('G : ', data.message);
-            message_error_in.innerText = data.message;
-            message_error_in.style.color = "red";
+
+            message_error.innerText = data.message;
+            message_error.style.color = "red";
         }
     } else {
-        alert('Tous les champs sont réquis');
+        // alert('Tous les champs sont réquis');
         let err_msg = "Tous les champs sont réquis";
         const message_error = document.getElementById('message_error');
         if (message_error) {
-            message_error_in.innerText = err_msg;
+            message_error.innerText = err_msg;
         }else {
             alert('Message error not found');
         } 
@@ -47,6 +48,7 @@ document.getElementById('inscription_page_submit').addEventListener('submit', as
     const email = document.getElementById('email_signup').value;
     const phone = document.getElementById('phone').value;
     const default_mot_de_passe =  document.getElementById('password').value;
+    const message_error_in = document.getElementById('message_error_in');
 
 
     if (nom != "" && prenom != ""  && email != "" && phone != "" && default_mot_de_passe != "") {
@@ -62,15 +64,14 @@ document.getElementById('inscription_page_submit').addEventListener('submit', as
         if (data.success) {
             window.location.href = '/users/mon-profile';
         } else {
-            const message_error = document.getElementById('message_error');
-            message_error.innerText = data.message;
+            message_error_in.innerText = data.message;
         }
     } else {
-        // alert('Tous les champs sont réquis');
+        alert('Tous les champs sont réquis');
         let err_msg = "Tous les champs sont réquis";
         const message_error = document.getElementById('message_error');
         if (message_error) {
-            message_error.innerText = err_msg;
+            message_error_in.innerText = err_msg;
         }else {
             alert('Message error not found');
         } 
