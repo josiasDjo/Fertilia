222cb7dd14e302b0191f0619ea655fa4bd058378
Josias Besodia <djodev23@gmail.com>
2025-03-04 19:30:57 +0100

    change31

--- backend/models/index.js
+++ backend/models/index.js
@@ -7,7 +7,7 @@ const sequelize = new Sequelize(
     process.env.DB_USER,
     process.env.DB_PASSWORD,
     {
-        host: process.env.DB_HOST,
+        host: process.env.DB_HOST || process.env.DB_HOST2,
         dialect: 'mysql', // Indique que l'on utilise MySQL
         logging: false, // Désactive les logs SQL dans la console
         pool: {
