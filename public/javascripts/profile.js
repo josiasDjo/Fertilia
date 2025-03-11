974e11d43220259bddbb051ac1e66d9a7946a0b2
Josias Besodia <djodev23@gmail.com>
2025-03-11 19:27:27 +0100

    change8

--- public/javascripts/profile.js
+++ public/javascripts/profile.js
@@ -120,59 +120,33 @@ if(form_add_field) {
         }
     });
 }
-async function field_mngt(event) {
-    let fields_container = document.getElementById('container_fields');
-    let not_element = document.getElementById('not_element');
-    // const response = await fetch("/api/champs/terrain/getAll", {
-    //     method: "GET",
-    //     headers: { "Content-Type": "application/json" }
-    // });
-    const fields = await response.json();
+// async function field_mngt(event) {
+//     let fields_container = document.getElementById('container_fields');
+//     let not_element = document.getElementById('not_element');
+//     // const response = await fetch("/api/champs/terrain/getAll", {
+//     //     method: "GET",
+//     //     headers: { "Content-Type": "application/json" }
+//     // });
+//     const fields = await response.json();
 
-    if (fields.length > 0) {
-        not_element.classList.remove('flex');
-        not_element.classList.add('hidden');
+//     if (fields.length > 0) {
+//         not_element.classList.remove('flex');
+//         not_element.classList.add('hidden');
 
-        fields_container.innerHTML = "";
-        console.log({fields});
-        fields.forEach(element => {
-            const articleHTML = `
-                <div id="field" class="w-full h-64 lg:h-64 lg:w-64 mx-2 my-10 lg:m-3 bg-gray-200 shadow-lg rounded-lg overflow-hidden flex flex-col relative">
-                    <div id="show_details" onclick="detail_me();" class="backdrop-blur bg-white h-10 w-10 absolute top-2 right-2 z-30 rounded-lg py-2 text-center justify-center items-center flex">
-                        <button type="button" id="show_details" class="btn_show_option h-full w-full text-xl text-center justify-center items-center flex"><i class="fa-solid fa-ellipsis-vertical"></i></button>
-                    </div>    
-                    <div class="bg-gray-200 w-full h-4/12 my-3 relative z-10">
-                        <h2 class="font-bold text-xl">${element.nom} </h2>
-                        <p>${element.surface} hectare</p>
-                    </div>
+//         fields_container.innerHTML = "";
+//         console.log({fields});
+//         fields.forEach(element => {
+//             const articleHTML = `
 
-                    <div id="graphic" class="w-full h-7/12 relative justify-center items-center flex my-3">
-                        <div class="w-16 items-center justify-center flex flex-col m-2">
-                            <div class="h-12 w-12 rounded-full border-4 border-solid border-yellow-500 text-center items-center justify-center flex">100</div>
-                            <p class="font-medium text-black">Humidité</p>
-                        </div>
-                        <div class="w-16 items-center justify-center flex flex-col m-2">
-                            <div class="h-12 w-12 rounded-full border-4 border-solid border-yellow-500 text-center items-center justify-center flex">100</div>
-                            <p class="font-medium text-black">Humidité</p>
-                        </div>                        
-                        <div class="w-16 items-center justify-center flex flex-col m-2">
-                            <div class="h-12 w-12 rounded-full border-4 border-solid border-yellow-500 text-center items-center justify-center flex">100</div>
-                            <p class="font-medium text-black">Humidité</p>
-                        </div>
-                    </div>
-                    <div class="w-full px-3">
-                        <button class="w-full backgroud_btn_h text-white rounded-xl my-5 p-2 border-2 border-solid border-yellow-500 border-opacity-30">Irriger</button>
-                    </div>
-                </div>
-            `;
-            fields_container.innerHTML += articleHTML;
-            console.log('Element', element.nom);
-        });
-    } else {
-        not_element.classList.add('flex');
-        not_element.classList.hidden('hidden');
-    }
-}
+//             `;
+//             fields_container.innerHTML += articleHTML;
+//             console.log('Element', element.nom);
+//         });
+//     } else {
+//         not_element.classList.add('flex');
+//         not_element.classList.hidden('hidden');
+//     }
+// }
 async function signout() {
     const etat = "Se deconnecter";
     const response = await fetch("/logout", {
