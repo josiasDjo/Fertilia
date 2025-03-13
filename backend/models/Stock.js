5866cb158b1a35e8b932ca59a3afe13f0de7710c
Josias Besodia <djodev23@gmail.com>
2025-03-13 20:45:24 +0100

    ~change28

--- backend/models/Stock.js
+++ backend/models/Stock.js
@@ -6,13 +6,16 @@ const Stock = sequelize.define('table_stocks', {
     id_stock: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
     utilisateur_id: { type: DataTypes.INTEGER, allowNull: false },
     produit: { type: DataTypes.STRING, allowNull: false },
+    type_produit: { type: DataTypes.STRING, allowNull: false },
     quantite: { type: DataTypes.DECIMAL, allowNull: false },
     unite: { type: DataTypes.STRING, allowNull: false },
+    emplacement: { type: DataTypes.STRING, allowNull: false },
+    fournisseur: { type: DataTypes.STRING, allowNull: false },
 }, {
     timestamp: true
 });
 
-Stock.belongsTo(Utilisateur, { foreignKey: 'utilisateur_id'});
-Utilisateur.hasMany(Stock, { foreignKey: 'id_utilisateurs' });
+Stock.belongsTo(Utilisateur, { foreignKey: 'utilisateur_id '});
+Utilisateur.hasMany(Stock, { foreignKey: 'utilisateur_id ' });
 
 module.exports = Stock;
