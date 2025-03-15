50724d196110afcfd1b2f9dd92d7a31f09c21ec4
Josias Besodia <djodev23@gmail.com>
2025-03-15 12:01:34 +0100

    change16

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -348,7 +348,9 @@ if (modify_product_on_stock) {
             let cells = row.getElementsByTagName('td');
 
             // Assigne les valeurs aux champs du formulaire
-            document.getElementById('id_product_modify_M').value = 
+            const id = document.getElementById('id_product_modify').textContent;
+            console.log('ID : ', id);
+            document.getElementById('id_product_modify_M').value = id;
             document.getElementById('nom_produit_modify').value = cells[1].textContent.trim();
             document.getElementById('quantite_produit_modify').value = cells[3].textContent.trim();
             document.getElementById('Emplacement_stock_modify').value = cells[4].textContent.trim();
