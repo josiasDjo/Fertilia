46279897a7cb6802effad15507e908f7463d19bb
Josias Besodia <djodev23@gmail.com>
2025-03-18 11:40:02 +0100

    change40

--- backend/controllers/EntreeSortieController.js
+++ backend/controllers/EntreeSortieController.js
@@ -18,11 +18,14 @@ exports.getEntreeSortie = async (req, res) => {
         const { id_utilisateurs } = req.body.users_id;
 
         const actionExiste = await EntreeSortie.findAll({ where: { id_utilisateurs: id_utilisateurs }});
-        if (!actionExiste) return res.json("Vous n'avez pas d'historique pour le moment");
-        return res.json(actionExiste);
+        if (!actionExiste) return "Vous n'avez pas d'historique pour le moment";
+        return actionExiste;
     } catch (err) {
         console.log('Une erreur s\'est produite : ', err);
-        return res.json({ success: false, message: 'Une erreur s\'est produite'});
+        return { 
+            success: false, 
+            message: 'Une erreur s\'est produite'
+        };
     }
 }
 
