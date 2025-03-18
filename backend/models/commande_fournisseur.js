844591771a3c38ce5383aac2b1b43c9569f6168e
Josias Besodia <djodev23@gmail.com>
2025-03-18 10:03:05 +0100

    change19

--- backend/models/commande_fournisseur.js
+++ backend/models/commande_fournisseur.js
@@ -2,3 +2,7 @@ const { DataTypes } = require('sequelize')
 const sequelize = require('./index');
 const Fournisseur = require('./Fournisseur');
 const Utilisateur = require('./Utilisateurs');
+
+const Commande_Founisseur = sequelize.define('table_commande_fournisseur', {
+    
+})
