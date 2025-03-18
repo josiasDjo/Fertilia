b466a88a10f7a3c224ef5e7e545c95a71b3bfd64
Josias Besodia <djodev23@gmail.com>
2025-03-18 09:38:22 +0100

    change11

--- backend/controllers/fournisseursControlleur.js
+++ backend/controllers/fournisseursControlleur.js
@@ -7,22 +7,47 @@ exports.createFournisseur = async (req, res) => {
         const FournisseurExiste = await Fournisseur.findOne({where: { nom_fournisseur: id_utilisateurs }});
         if (FournisseurExiste) return res.json({ success: false, message: 'Ce fournisseur existe déjà !! '});
 
-        const NouveauFournisseur = await Fournisseur.create()
+        const NouveauFournisseur = await Fournisseur.create({ nom_fournisseur, contact_fournisseur, id_utilisateurs });
+        console.log('Nouvel fournisseur : ', NouveauFournisseur);
+        return res.json({ success: true, message: 'Fournisseur ajouté avec succès !! '});
     } catch(err) {
-
+        console.log('Une erreur s\'est produite lors de la création du fournisseur : ', err);
+        return res.json({ success: false, message: 'Une erreur s\'est produite lors de la création du fournisseur'});
     }
 }
 
 exports.getAllFournisseur = async (req, res) => {
-
+    try {
+        const id_utilisateur = req.body.id_utilisateur;
+        const Fournisseurs = await Fournisseur.findAll({ where: { id_utilisateurs: id_utilisateur }});
+        
+        if (!Fournisseurs) return res.json({ success: false, message: 'Aucun fournisseur trouvé ! '});
+        return res.json({Fournisseurs});
+    } catch(err) {
+        console.log('Une erreur s\'est produite : ', err);
+        res.json({ success: false, message: 'Une erreur s\'est produite !! '});
+    }
 }
 
 exports.getFournisseur = async (req, res) => {
-
+    try {
+        const { id_utilisateur, nom_fournisseur } = req.body;
+        
+        const Fournisseur_one = await Fournisseur.findOne({ where: { id_utilisateur, nom_fournisseur}});
+        if (!Fournisseur_one) return res.json({ success: false, message: 'Auncun fournisseur trouvé'});
+        return res.json({Fournisseur_one});
+    } catch (err) {
+        console.log('Une erreur s\'est produite : ', err);
+        res.json({ success: false, message: 'Une erreur d\'est produite'});
+    }
 }
 
 exports.modifyFournisseur = async (req, res) => {
+    try {
 
+    } catch {
+        
+    }
 }
 
 exports.deleteFournisseur = async (req, res) => {
