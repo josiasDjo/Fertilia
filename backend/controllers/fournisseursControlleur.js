f27a6eef0d848e188e7c42ef08919b4eef6120f3
Josias Besodia <djodev23@gmail.com>
2025-03-18 09:23:06 +0100

    change10

--- backend/controllers/fournisseursControlleur.js
+++ backend/controllers/fournisseursControlleur.js
@@ -1,7 +1,16 @@
 const Fournisseur = require('../models/Fournisseur');
 
 exports.createFournisseur = async (req, res) => {
-    
+    try {
+        const { nom_fournisseur, contact_fournisseur, id_utilisateurs } =  req.body;
+
+        const FournisseurExiste = await Fournisseur.findOne({where: { nom_fournisseur: id_utilisateurs }});
+        if (FournisseurExiste) return res.json({ success: false, message: 'Ce fournisseur existe déjà !! '});
+
+        const NouveauFournisseur = await Fournisseur.create()
+    } catch(err) {
+
+    }
 }
 
 exports.getAllFournisseur = async (req, res) => {
