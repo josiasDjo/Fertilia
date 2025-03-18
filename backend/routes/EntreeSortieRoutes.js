eb6c7c6252b2022218f56fcf039d835383933c8b
Josias Besodia <djodev23@gmail.com>
2025-03-18 10:48:34 +0100

    change28

---
+++ backend/routes/EntreeSortieRoutes.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const router = express.Router();
+const EntreeSortieController = require('');
+
+router.post('', EntreeSortieController);
+router.get('', EntreeSortieController);
+router.put('', EntreeSortieController);
+router.delete('', EntreeSortieController);
