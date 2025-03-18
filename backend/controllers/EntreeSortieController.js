5b2bead170479f51e922c0a0a21a68eca42fcd29
Josias Besodia <djodev23@gmail.com>
2025-03-18 10:58:18 +0100

    change33

--- backend/controllers/EntreeSortieController.js
+++ backend/controllers/EntreeSortieController.js
@@ -5,6 +5,8 @@ exports.ajouterEntreeSortie = async (req, res) => {
     try {
         const { produit, type_produit, quantite, unite, action, quantite_totale, id_utilisateurs } = req.body;
         
+        await EntreeSortie.create({produit, type_produit, quantite, unite, action, quantite_totale, id_utilisateurs});
+        return res.json({ success: true, message: 'Action enregistrer'});
     } catch(err) {
         console.log('Une erreur s\'est produite : ', err);
         return res.json({ success: false, message: 'Une erreur s\'est produite'});
