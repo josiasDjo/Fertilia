6330eeddd96a4394af963720841e459bab1ce35f
Josias Besodia <djodev23@gmail.com>
2025-03-18 09:00:27 +0100

    change2

--- backend/models/Fournisseur.js
+++ backend/models/Fournisseur.js
@@ -5,4 +5,7 @@ const Fournisseur = sequelize.define('table_fournisseur', {
     id_fournisseur : { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
     nom_fournisseur: { type: DataTypes.STRING, allowNull: false },
     contact_fournisseur: { type: DataTypes.STRING, allowNull: false },
+    id_utilisateurs : { type: DataTypes.INTEGER, allowNull: false },
+}, {
+    
 })
