1a2a3aa1fca24230030720a81c4991f4c2e82067
Josias Besodia <djodev23@gmail.com>
2025-03-18 08:58:13 +0100

    change1

--- backend/models/Fournisseur.js
+++ backend/models/Fournisseur.js
@@ -1,5 +1,8 @@
 const sequelize = require('./index');
+const { DataTypes, INTEGER } = require('sequelize');
 
 const Fournisseur = sequelize.define('table_fournisseur', {
-    
+    id_fournisseur : { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+    nom_fournisseur: { type: DataTypes.STRING, allowNull: false },
+    contact_fournisseur: { type: DataTypes.STRING, allowNull: false },
 })
