aba517d438a31c47f31adff70aebe320fe9746a5
Josias Besodia <djodev23@gmail.com>
2025-03-18 09:18:15 +0100

    change8

--- backend/routes/FournisseurRoutes.js
+++ backend/routes/FournisseurRoutes.js
@@ -2,9 +2,10 @@ const express = require('express');
 const router = express.Router();
 const FournisseurRoutes = require('../controllers/fournisseursControlleur');
 
-router.post('/ajouter-fournisseur');
-router.get('/');
-router.put('/modifier-fournisseur');
-router.delete('/supprimer-fournisseur');
+router.post('/ajouter-fournisseur', FournisseurRoutes.createFournisseur);
+router.get('/', FournisseurRoutes.getAllFournisseur);
+router.get('/fournisseur', FournisseurRoutes.getFournisseur)
+router.put('/modifier-fournisseur', FournisseurRoutes.modifyFournisseur);
+router.delete('/supprimer-fournisseur', FournisseurRoutes.deleteFournisseur);
 
 module.exports = router;
