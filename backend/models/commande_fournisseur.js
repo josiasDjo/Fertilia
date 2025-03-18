b98ec8d04c8d4edeb8a2e1263fe19fefd8e8a03e
Josias Besodia <djodev23@gmail.com>
2025-03-18 10:05:57 +0100

    change20

--- backend/models/commande_fournisseur.js
+++ backend/models/commande_fournisseur.js
@@ -3,6 +3,10 @@ const sequelize = require('./index');
 const Fournisseur = require('./Fournisseur');
 const Utilisateur = require('./Utilisateurs');
 
-const Commande_Founisseur = sequelize.define('table_commande_fournisseur', {
-    
+const Commande_Fournisseur = sequelize.define('table_commande_fournisseur', {
+    id_commande: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+    produit: { type: DataTypes.STRING, allowNull: false },
+    type_produit: { type: DataTypes.STRING, allowNull: false },
+    quantite: { type: DataTypes.FLOAT, allowNull: false },
+    unite: { }
 })
