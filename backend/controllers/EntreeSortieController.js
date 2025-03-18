e07df1bda23118059a0812ddce1a9b68a56decc7
Josias Besodia <djodev23@gmail.com>
2025-03-18 10:53:57 +0100

    change31

--- backend/controllers/EntreeSortieController.js
+++ backend/controllers/EntreeSortieController.js
@@ -2,7 +2,12 @@ const EntreeSortie = require('../models/Entree_Sortie');
 
 
 exports.ajouterEntreeSortie = async (req, res) => {
+    try {
 
+    } catch(err) {
+        console.log('Une erreur s\'est produite : ', err);
+        return res.json({ success: false, message: 'Une erreur s\'est produite'});
+    }
 }
 
 exports.getEntreeSortie = async (req, res) => {
