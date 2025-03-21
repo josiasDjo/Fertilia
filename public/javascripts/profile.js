12cc0a9864ba8318477450efcdc653dc64f3a3f6
Josias Besodia <djodev23@gmail.com>
2025-03-21 07:28:21 +0100

    change48

--- public/javascripts/profile.js
+++ public/javascripts/profile.js
@@ -145,7 +145,7 @@ if(form_add_products) {
 
             if (data.success) {
                 const action = "Entrée";
-                const quantite_initiale = 
+                const quantite_initiale = quantite;
                 const History = EntreeSortieHistory(produit,type_produit,quantite,quantite_initiale,unite,action);
 
                 message_show.innerText = data.message;
