bd5b5846ead76bedd2fc1bcf8cc4a15466ea3ff0
Josias Besodia <djodev23@gmail.com>
2025-04-28 18:15:13 +0200

    change3

--- backend/controllers/capteursEquipementController.js
+++ backend/controllers/capteursEquipementController.js
@@ -4,7 +4,8 @@ const { v4: uuidv4 } = require('uuid');
 exports.addCapteur = async (req, res) => {
     try {
         const id_capteur = uuidv4()
-        const { nom_capteur,type_capteur,model_capteur,numero_de_serie,is_champs,id_utilisateurs,latitude,longitude,date_installation,statut,last_communication,photo } = req.body
+        const id_utilisateurs = req.session.users.id_user;
+        const { nom_capteur,type_capteur,model_capteur,numero_de_serie,is_champs,latitude,longitude,date_installation,statut,last_communication,photo } = req.body
         const serialNumberExist = await CapteurEquipement.findOne({where: {numero_de_serie}})
         if(serialNumberExist) return res.json({ success: false, message: 'Ce capteur existe déjà'})
         await CapteurEquipement.create({id_capteur,nom_capteur,type_capteur,model_capteur,numero_de_serie,is_champs,id_utilisateurs,latitude,longitude,date_installation,statut,last_communication,photo})
@@ -12,5 +13,15 @@ exports.addCapteur = async (req, res) => {
         const error = 'Erreur lors l\'ajout du capteur, réessayer plus tard'
         console.log(error, err)
         return res.json({ success: false, message: error })
+    }
+}
+
+exports.getCapteursChamps = async (req, res) => {
+    try {
+
+    } catch(err) {
+        const error = 'Erreur lors de la récupération des capteurs'
+        console.log(error, err)
+        return res.json({ success: false, message: error})
     }
 }
