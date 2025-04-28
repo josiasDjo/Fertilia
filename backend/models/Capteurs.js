68706259089cbda8192049c94613daa65c29fcd2
Josias Besodia <djodev23@gmail.com>
2025-04-28 10:04:22 +0200

    model capteur update

--- backend/models/Capteurs.js
+++ backend/models/Capteurs.js
@@ -6,7 +6,10 @@ const Capteur = sequelize.define('table_capteurs', {
     id_capteur: { type: DataTypes.INTEGER,  primaryKey: true, autoIncrement: true },
     champ_id: { type: DataTypes.INTEGER, allowNull: false }, 	
     type_capteur: { type: DataTypes.STRING, allowNull: false }, 	
-    valeur: { type: DataTypes.DECIMAL, allowNull: false }
+    valeur: { type: DataTypes.DECIMAL, allowNull: false },
+    unit: { type: DataTypes.STRING, allowNull: false },
+    longitude: { type: DataTypes.FLOAT, allowNull: true},
+    latitude: { type: DataTypes.FLOAT, allowNull: true}
 }, {
     timestamp: true
 });
