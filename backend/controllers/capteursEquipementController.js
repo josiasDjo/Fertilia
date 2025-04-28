e781023b7a65e8bb4bea62e100814ebb41c0b962
Josias Besodia <djodev23@gmail.com>
2025-04-28 18:09:36 +0200

    controller config

---
+++ backend/controllers/capteursEquipementController.js
@@ -0,0 +1,12 @@
+const CapteurEquipement = require('../models/capteurs_equipements')
+const { v4: uuidv4 } = require('uuid');
+
+exports.addCapteur = async (req, res) => {
+    try {
+        const { id_capteur,nom_capteur,type_capteur,model_capteur,numero_de_serie,is_champs,id_utilisateurs,latitude,longitude,date_installation,statut,last_communication,photo } = req.body
+    } catch(err) {
+        const error = 'Erreur lors l\'ajout du capteur, réessayer plus tard'
+        console.log(error, err)
+        return res.json({ success: false, message: error })
+    }
+}
