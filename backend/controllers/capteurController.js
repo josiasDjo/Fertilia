21c331a86f178652869ccb498e43b1463acba3e1
Josias Besodia <djodev23@gmail.com>
2025-04-28 12:16:17 +0200

    change4

--- backend/controllers/capteurController.js
+++ backend/controllers/capteurController.js
@@ -4,9 +4,8 @@ const { v4: uuidv4 } = require('uuid');
 exports.addCapteur = async (req, res) => {
     try {
         const id_capteur = uuidv4()
-        const { type_capteur,valeur,createdAt,updatedAt,is_champs,unit,longitude,latitude } = req.body;
-
-        
+        const { type_capteur,valeur,is_champs,unit,longitude,latitude } = req.body;
+        await Capteur.create({id_capteur,type_capteur,valeur,is_champs,unit,longitude,latitude})
     } catch(err) {
         console.log('Erreur lors de l\'ajout du champ', err)
         res.json({ success: false, message: 'Erreur lors de la création du champ' });
