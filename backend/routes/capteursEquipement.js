5f806e6776cbe668af6213c7b50a6957838aeeb7
Josias Besodia <djodev23@gmail.com>
2025-04-28 18:45:04 +0200

    change2

--- backend/routes/capteursEquipement.js
+++ backend/routes/capteursEquipement.js
@@ -2,10 +2,10 @@ const express = require('express')
 const router = express.Router()
 const CapteurEquipement = require('../controllers/capteursEquipementController')
 
-router.post('', CapteurEquipement.addCapteur)
-router.get('', CapteurEquipement.getCapteur)
-router.get('', CapteurEquipement.getCapteursChamps)
-router.put('', CapteurEquipement.updateCapteur)
-router.delete('', CapteurEquipement.deleteCapteur)
+router.post('/add', CapteurEquipement.addCapteur)
+router.get('/', CapteurEquipement.getCapteur)
+router.get('/all-senors', CapteurEquipement.getCapteursChamps)
+router.put('/modifier', CapteurEquipement.updateCapteur)
+router.delete('delete', CapteurEquipement.deleteCapteur)
 
 module.exports = router
