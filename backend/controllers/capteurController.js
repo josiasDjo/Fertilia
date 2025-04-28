66f5507edf02e582d1c93b6f6a2d7fa8e55a12ce
Josias Besodia <djodev23@gmail.com>
2025-04-28 12:13:58 +0200

    change3

--- backend/controllers/capteurController.js
+++ backend/controllers/capteurController.js
@@ -5,7 +5,10 @@ exports.addCapteur = async (req, res) => {
     try {
         const id_capteur = uuidv4()
         const { type_capteur,valeur,createdAt,updatedAt,is_champs,unit,longitude,latitude } = req.body;
-    } catch(err) {
 
+        
+    } catch(err) {
+        console.log('Erreur lors de l\'ajout du champ', err)
+        res.json({ success: false, message: 'Erreur lors de la création du champ' });
     }
 }
