01a1365e1cbfae1499fda220c8a2b117790cd2c1
Josias Besodia <djodev23@gmail.com>
2025-04-28 12:43:38 +0200

    change7

--- backend/controllers/capteurController.js
+++ backend/controllers/capteurController.js
@@ -15,9 +15,13 @@ exports.addCapteur = async (req, res) => {
 
 exports.getCapteur = async (req, res) => {
     try {
-
+        const id_capteur = req.body.id_capteur
+        const capteurExiste = await Capteur.findByPk(id_capteur)
+        if(!capteurExiste) return res.json({ success: false, message:'Capteur introuvable, réessayer plus tard'})
+        return res.json({ success:true, capteurExiste:capteurExiste })
     } catch(err) {
-        
+        console.log('Erreur lors de la récupération du capteur : ', err)
+        return res.json({success: false, message:'Erreur lors de la récupération du capteur'})
     }
 }
 
@@ -26,7 +30,7 @@ exports.updateCapteur = async (req, res) => {
         // const {id_capteur,type_capteur,valeur,is_champs,unit,longitude,latitude} = req.body;
         const id_capteur = req.body.id_capteur
         const capteurExiste = Capteur.findByPk(id_capteur)
-        if(!capteurExiste) return res.json({ success: false, message: 'Capteur introuvable, réessyer plus tard'})
+        if(!capteurExiste) return res.json({ success: false, message: 'Capteur introuvable, réessayer plus tard'})
         await Capteur.update(req.body)
         res.json({ success: true, message:'Capteur mise à jour avec succès'})
     } catch(err) {
