d369a1267e9282312aec520ae7a99b4683e91e16
Josias Besodia <djodev23@gmail.com>
2025-04-28 17:53:05 +0200

    CHANGE4 front

--- backend/controllers/v_cards_capteurs_champs.js
+++ backend/controllers/v_cards_capteurs_champs.js
@@ -3,10 +3,10 @@ const V_Card = require('../models/v_cards_capteurs_champs')
 exports.getAllCards = async (req, res) => {
     try {
         const utilisateur_id = req.users_id
-        const champs = await V_Card.findAll({where: {utilisateur_id:utilisateur_id}})
-        if(!champs) return {success: false, message: 'Aucun élément trouvé'}
-        console.log(champs)
-        return champs
+        const v_champs = await V_Card.findAll({where: {utilisateur_id:utilisateur_id}})
+        if(!v_champs) return {success: false, message: 'Aucun élément trouvé'}
+        console.log(v_champs)
+        return v_champs
     } catch (err) {
         console.log('Erreur lors de la récupération des cards', err)
         return {success: false, message: 'Erreur lors de la récupération des cards'}
