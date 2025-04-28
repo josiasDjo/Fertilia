5e47dd369c111ddb9e1c5ad353753334fb404b37
Josias Besodia <djodev23@gmail.com>
2025-04-28 17:11:00 +0200

    bug set

--- backend/controllers/v_cards_capteurs_champs.js
+++ backend/controllers/v_cards_capteurs_champs.js
@@ -3,9 +3,9 @@ const V_Card = require('../models/v_cards_capteurs_champs')
 exports.getAllCards = async (req, res) => {
     try {
         const utilisateur_id = req.users_id
-        const allCards = await V_Card.findAll({where: {utilisateur_id}})
-        if(!allCards) return {success: false, message: 'Aucun élément trouvé'}
-        return { success: true, message: allCards}
+        const champs = await V_Card.findAll({where: {utilisateur_id}})
+        if(!champs) return {success: false, message: 'Aucun élément trouvé'}
+        return champs
     } catch (err) {
         console.log('Erreur lors de la récupération des cards')
         return {success: false, message: 'Erreur lors de la récupération des cards'}
