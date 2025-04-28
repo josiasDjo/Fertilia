fb6e27c403802fd96b17049e476bb40140cd355d
Josias Besodia <djodev23@gmail.com>
2025-04-28 18:12:45 +0200

    change2

--- backend/controllers/capteursEquipementController.js
+++ backend/controllers/capteursEquipementController.js
@@ -3,8 +3,10 @@ const { v4: uuidv4 } = require('uuid');
 
 exports.addCapteur = async (req, res) => {
     try {
-        const { id_capteur,nom_capteur,type_capteur,model_capteur,numero_de_serie,is_champs,id_utilisateurs,latitude,longitude,date_installation,statut,last_communication,photo } = req.body
-
+        const id_capteur = uuidv4()
+        const { nom_capteur,type_capteur,model_capteur,numero_de_serie,is_champs,id_utilisateurs,latitude,longitude,date_installation,statut,last_communication,photo } = req.body
+        const serialNumberExist = await CapteurEquipement.findOne({where: {numero_de_serie}})
+        if(serialNumberExist) return res.json({ success: false, message: 'Ce capteur existe déjà'})
         await CapteurEquipement.create({id_capteur,nom_capteur,type_capteur,model_capteur,numero_de_serie,is_champs,id_utilisateurs,latitude,longitude,date_installation,statut,last_communication,photo})
     } catch(err) {
         const error = 'Erreur lors l\'ajout du capteur, réessayer plus tard'
