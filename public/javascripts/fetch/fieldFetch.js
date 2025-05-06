a29e8c3a81d048dda59bab8ccac342b4700def43
Josias Besodia <djodev23@gmail.com>
2025-05-06 14:46:03 +0200

    change4

--- public/javascripts/fetch/fieldFetch.js
+++ public/javascripts/fetch/fieldFetch.js
@@ -1,5 +1,11 @@
-function getFields(sort = "all") {
-    const response = {
-        
+async function getFields(sort = "all") {
+    const response = await fetch("/api/champs/terrain/getAll", {
+        method: "GET",
+        headers: { "Content-Type": "application/json" },
+    })
+    const data = await response.json()
+
+    for(let i = 0; i < data.length; i++) {
+        console.log(data[i])
     }
 }
