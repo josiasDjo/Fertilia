5fb754fdd02db024e18f2cb755f975b48e9c204d
Josias Besodia <djodev23@gmail.com>
2025-05-07 14:37:17 +0200

    change2

--- backend/middlewares/authenticateWithJsonToken.js
+++ backend/middlewares/authenticateWithJsonToken.js
@@ -2,7 +2,11 @@ const jwt = require('jsonwebtoken')
 
 const token = jwt.sign(
     {
-        user_id: user_id, email: email, role_id: role_id
+        user_id: user_id,
+        nom: nom,
+        prenom: prenom,
+        email: email, 
+        role_id: role_id
     }, process.env.JWT_SECRET,
     {
         expiresIn: '14j'
